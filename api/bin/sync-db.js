a6eaa8dcb4d28a2a4b8b8591f23abe1f54e9252b
Hojun Hwang <ho.jun.hwang@umontreal.ca>
2023-10-31 16:22:28 -0400

    force options modified

--- api/bin/sync-db.js
+++ api/bin/sync-db.js
@@ -3,5 +3,8 @@ const models = require("../../models");
 // force:true 는 기존에 db가 있더라도 데이터를 다 없애고 새로 시작하는 옵션
 
 module.exports = () => {
-  return models.sequelize.sync({ force: true });
+  const options = {
+    force: process.env.NODE_ENV === "test" ? true : false,
+  };
+  return models.sequelize.sync(options);
 };
